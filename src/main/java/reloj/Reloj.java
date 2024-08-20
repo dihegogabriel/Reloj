4d2e7ba8bbc67590310a1f31912102f934a5df15
dihegogabriel <diegabrielgallardo@gmail.com>
2024-08-20 20:54:50 -0300

    Initial commit

---
+++ src/main/java/reloj/Reloj.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package reloj;
+
+/**
+ *
+ * @author diego
+ */
+public class Reloj {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
